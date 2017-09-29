5dc4c2859e46289423699b4b600536764a5e06f1
Xenoveritas <dmpotter@xenoveritas.org>
2017-09-28 23:16:00 -0400

    Add some more example items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Seed the database.
 
-# Create a sample event.
-
+# Create a sample event. This is important because there needs to be a
+# "current event."
 Event.create! name: "Sample Event", location: "Desk",
   start_date: Date.today, end_date: 1.week.from_now,
   current: true
@@ -9,8 +9,13 @@ Event.create! name: "Sample Event", location: "Desk",
 # Create some sample games. First up, make up an example publisher and an
 # example title:
 publisher = Publisher.create! name: "Example Publisher"
-title = Title.create! title: "Example Card Game", publisher: publisher
+card_game = Title.create! title: "Example Card Game", publisher: publisher
+tournament_game = Title.create! title: "Example Tournament Game", publisher: publisher, likely_tournament: true
 
 # Now create example instances:
-Game.create! barcode: "BARCODE1", title: title
-Game.create! barcode: "BARCODE2", title: title
+Game.create! barcode: "BARCODE1", title: card_game
+Game.create! barcode: "BARCODE2", title: card_game
+Game.create! barcode: "BARCODE3", title: tournament_game
+Game.create! barcode: "BARCODE4", title: tournament_game
+Game.create! barcode: "BARCODE5", title: tournament_game
+Game.create! barcode: "BARCODE6", title: tournament_game
